0a40494f3b8bbf8fec6d8de76eb42530d770f529
Cojan van Ballegooijen <cvanball@redhat.com>
2017-03-03 00:00:28 +0100

    Initial changes

--- setup.sh
+++ setup.sh
@@ -3,11 +3,14 @@ oc login -u system:admin
 oc delete -n openshift -f jboss-image-streams.json
 oc create -n openshift -f jboss-image-streams.json
 oc -n openshift import-image jboss-datavirt63-openshift --insecure=true
+oc -n openshift import-image jboss-datagrid65-openshift --insecure=true
 oc login -u developer -p developer
 echo 'Creating a new project called churnprediction'
 oc new-project churnprediction
 echo 'Creating a service account and accompanying secret for use by the datavirt application'
 oc create -f datavirt-app-secret.yaml
+oc secret new resourceadapters-secret resourceadapters.properties
+oc secret new configure-secret postconfigure.sh
 echo 'Add the role view to the service account under which the pod is running'
 oc adm policy add-role-to-user view system:serviceaccount:churnprediction:datavirt-service-account
 echo 'Deploying churnprediction template with default values and JDV with JDG'
